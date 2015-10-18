02705178f695639cd99b4f0b7fb92a6fa1172d41
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-17 22:39:52 -0700

    Update

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Course do
-<<<<<<< HEAD
 
   describe "#initialize" do
     it "should not be nil when creating new instance" do
@@ -16,63 +15,31 @@ describe Course do
 
     it "should create new instances with checkpoints" do
       course = Course.new("Rails Web Development", 55)
-=======
-  
-  describe "#initialize" do
-    it "should not be nil when creating new instance" do
-      course = Course.new(name: "Rails Web Development", checkpoints: 55)
-      expect(course).not_to be(nil)
-    end
-    
-    it "should create new instances with name" do
-      course = Course.new(name: "Rails Web Development", checkpoints: 55)
-      expect(course.name).to eq("Rails Web Development")
-    end
-    
-    it "should create new instances with checkpoints" do
-      course = Course.new(name: "Rails Web Development", checkpoints: 55)
->>>>>>> 658e4a71fdf5a0de9e6076f6af0a51be60f36f85
       expect(course.checkpoints).to eq(55)
     end
   end
 
   describe "attributes" do
     it "should respond to name" do
-<<<<<<< HEAD
       course = Course.new("Rails Web Development", 55)
-=======
-      course = Course.new(name: "Rails Web Development", checkpoints: 55)
->>>>>>> 658e4a71fdf5a0de9e6076f6af0a51be60f36f85
       expect(course).to respond_to(:name)
     end
 
     it "should respond to checkpoints" do
-<<<<<<< HEAD
       course = Course.new("Rails Web Development", 55)
-=======
-      course = Course.new(name: "Rails Web Development", checkpoints: 55)
->>>>>>> 658e4a71fdf5a0de9e6076f6af0a51be60f36f85
       expect(course).to respond_to(:checkpoints)
     end
   end
 
   describe "actions" do
     it "#complete_checkpoint" do
-<<<<<<< HEAD
       course = Course.new("Rails Web Development", 55)
-=======
-      course = Course.new(name: "Rails Web Development", checkpoints: 55)
->>>>>>> 658e4a71fdf5a0de9e6076f6af0a51be60f36f85
       course.complete_checkpoint
       expect(course.checkpoints).to eq(54)
     end
 
     it "#remaining_checkpoints" do
-<<<<<<< HEAD
       course = Course.new("Rails Web Development", 55)
-=======
-      course = Course.new(name: "Rails Web Development", checkpoints: 55)
->>>>>>> 658e4a71fdf5a0de9e6076f6af0a51be60f36f85
       course.complete_checkpoint
       expect(course.remaining_checkpoints).to eq(54)
     end
